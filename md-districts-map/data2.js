81fe4b7ee0ce105e78b4cd5f4fbae9e03086ade8
Adam <adam@adammarton.com>
2019-10-23 20:37:27 -0400

    pa maps

---
+++ md-districts-map/data2.js
@@ -0,0 +1,36 @@
+var data2 =
+{
+    "01": {
+    "district": 1,
+    "rep": "Andy Harris (R-Cockeysville)",
+    },
+    "02": {
+    "district": 2,
+    "rep": "Dutch Ruppersberger (D-Cockeysville)",
+    },
+    "03": {
+    "district": 3,
+    "rep": "John Sarbanes (D-Towson) ",
+  },
+    "04": {
+    "district": 4,
+    "rep": "Anthony G. Brown (D-Bowie)",
+    },
+    "05": {
+    "district": 5,
+    "rep": "Steny Hoyer (D-Mechanicsville) ",
+    },
+    "06": {
+    "district": 6,
+    "rep": "David Trone (D-Potomac)",
+    },
+    "07": {
+    "district": 7,
+    "rep": "Vacant",
+    },
+    "08": {
+    "district": 8,
+    "rep": "Jamie Raskin (D-Takoma Park)",    
+    }
+
+}
---
+++ pa-map/data2.js
@@ -0,0 +1,36 @@
+var data2 =
+{
+    "ADAMS": {
+    "total": 172,
+    "total_resentanced": 300,
+    "released": 22,
+    "resentanced_nonlife": 983,
+    "given_life": 172,
+    "waiting_resentance": 11
+    },
+    "ALLEGHENY": {
+    "total": 121,
+    "total_resentanced": 15,
+    "released": 9,
+    "resentanced_nonlife": 33,
+    "given_life": 354,
+    "waiting_resentance": 22
+    },
+    "ARMSTRONG": {
+    "total": 566,
+    "total_resentanced": 124,
+    "released": 172,
+    "resentanced_nonlife": 22,
+    "given_life": 90,
+    "waiting_resentance": 81
+  },
+
+    "BEAVER": {
+    "total": 0,
+    "total_resentanced": 0,
+    "released": 0,
+    "resentanced_nonlife": 0,
+    "given_life": 0,
+    "waiting_resentance": 0
+    }
+}
